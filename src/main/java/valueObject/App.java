a6394829dd9d8fb6364aa9ade4b79f4028a62110
Мираев Давид Николаевич <Dmiraev@synergy.ru>
2023-09-15 13:33:29 +0300

    Add App without usage of value-object pattern

--- src/main/java/valueObject/App.java
+++ src/main/java/valueObject/App.java
@@ -3,91 +3,74 @@ package valueObject;
 import java.util.regex.Pattern;
 
 public class App {
-    public static String registration(User user){
-        String usernamevalidateResult = validateUsername(user.getUsername());
-        String passwordvalidateResult = validatePassword(user.getPassword());
-        String emailvalidateResult = validateEmail(user.getEmail());
-        String agevalidateResult = validateAge((user.getAge()));
-
+    public static String registration(int age, String username, String password, String email) {
         String resultMessage = "Guest wasn't registered.";
-        if (!(usernamevalidateResult == null)) {
-            resultMessage = resultMessage + " | " + usernamevalidateResult;
-        }
-        if (!(passwordvalidateResult == null)) {
-            resultMessage = resultMessage + " | " + passwordvalidateResult;
-        }
-        if (!(emailvalidateResult == null)) {
-            resultMessage = resultMessage + " | " + emailvalidateResult;
-        }
-        if (!(agevalidateResult == null)) {
-            resultMessage = resultMessage + " | " + agevalidateResult;
-        }
-        if (resultMessage == "Guest wasn't registered.") {
-            resultMessage = "User was registered.";
-        }
+        String emailCheckResult = "";
+        String passwordCheckResult = "";
+        String usernameCheckResult = "";
+        String ageCheckResult = "";
 
-        System.out.println(resultMessage);
-
-        return resultMessage;
-    }
-    
-    private static String validateEmail(String email){
         if (email == null) {
-            return "Поле email null";
+            emailCheckResult = emailCheckResult + " | " + "Поле email null";
         }
         else if (email == "") {
-            return "Поле email пустое";
+            emailCheckResult = emailCheckResult + " | " + "Поле email пустое";
         }
         else if (!email.contains("@")){
-            return "Поле email не содержит спецсимвола @";
+            emailCheckResult = emailCheckResult + " | " + "Поле email не содержит спецсимвола @";
         }
         else if (Pattern.matches(".*\\p{InCyrillic}.*", email)){
-            return  "Поле email содержит символы кириллицы";
+            emailCheckResult = emailCheckResult + " | " + "Поле email содержит символы кириллицы";
         }
         else {
-            return null;
+            emailCheckResult = null;
         }
-    }
 
-    private static String validatePassword(String password){
         if (password == null) {
-            return "Поле password null";
+            passwordCheckResult = passwordCheckResult + " | " + "Поле password null";
         }
         else if (password == "") {
-            return "Поле password пустое";
+            passwordCheckResult = passwordCheckResult + " | " + "Поле password пустое";
         }
         else if (password.length() < 8){
-            return "Поле password содержит менее 8 символов";
+            passwordCheckResult = passwordCheckResult + " | " + "Поле password содержит менее 8 символов";
         }
         else if (Pattern.matches(".*\\p{InCyrillic}.*", password)){
-            return  "Поле password содержит символы кириллицы";
+            passwordCheckResult = passwordCheckResult + " | " + "Поле password содержит символы кириллицы";
         }
         else {
-            return null;
+            passwordCheckResult = null;
         }
-    }
 
-    private static String validateUsername(String username){
         if (username == null) {
-            return "Поле username null";
+            usernameCheckResult = usernameCheckResult + " | " + "Поле username null";
         }
         else if (username == "") {
-            return "Поле username пустое";
+            usernameCheckResult = usernameCheckResult + " | " + "Поле username пустое";
         }
         else if (Pattern.matches(".*\\p{InCyrillic}.*", username)){
-            return  "Поле username содержит символы кириллицы";
+            usernameCheckResult = usernameCheckResult + " | " + "Поле username содержит символы кириллицы";
         }
         else {
-            return null;
+            usernameCheckResult = null;
         }
-    }
 
-    private static String validateAge(int age){
         if (age < 18){
-            return  "Поле age меньше 18";
+            ageCheckResult = ageCheckResult + " | " + "Поле age меньше 18";
         }
         else {
-            return null;
+            ageCheckResult = null;
+        }
+
+        if (usernameCheckResult == null && passwordCheckResult == null && emailCheckResult == null && ageCheckResult == null) {
+            resultMessage = "User was registered.";
+            System.out.println(resultMessage);
+            return resultMessage;
+        }
+        else {
+            resultMessage = resultMessage + usernameCheckResult + passwordCheckResult + emailCheckResult + ageCheckResult;
+            System.out.println(resultMessage);
+            return resultMessage;
         }
     }
 }
